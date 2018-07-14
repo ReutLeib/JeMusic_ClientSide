f9b0d5087659094baadc83eb5bdd0e220039dfe0
ReutLeib <leibreut@mail.com>
2018-07-14 12:49:34 +0300

    update css

--- src/Components/NewSubject.js
+++ src/Components/NewSubject.js
@@ -149,7 +149,7 @@ class InsertSubject extends Component {
                         requiredSkills:<br></br>
                         <input onChange={this.handleRequiredSkills} value={this.state.newRequiredSkills} type="text" name="requiredSkills" />
                       </label><br></br>
-                   <button type="submit" className="btn btn-primary insertSub removeHover" onClick={this.delete}><FaCaretSquareORight size={45} background={'#666666'} className="greenElement borderBlack" /> </button> 
+                   <button type="submit" className="btn btn-primary insertSub removeHover" onClick={this.delete}><FaCaretSquareORight size={45} background={'#666666'} className="greenElement" /> </button> 
 
                 </form>
                 <div id="response">
